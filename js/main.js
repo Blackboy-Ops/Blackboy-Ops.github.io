7eb092d15ae969473cfe727aa748661af182c523
Samuel Muriithi <42814383+Blackboy-Ops@users.noreply.github.com>
2023-02-06 11:33:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* ===================================================================
- * Epitome - Main JS
+ *  Main JS
  *
  * ------------------------------------------------------------------- */
 
@@ -14,8 +14,7 @@
 
     $WIN = $(window);
 
-    // Add the User Agent to the <html>
-    // will be used for IE10/IE11 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; rv:11.0))
+
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
 
@@ -28,16 +27,16 @@
 
         $WIN.on('load', function() {
 
-            //force page scroll position to top at page refresh
+            
             $('html, body').animate({ scrollTop: 0 }, 'normal');
 
-            // will first fade out the loading animation 
+            //  loading animation 
             $("#loader").fadeOut("slow", function() {
                 // will fade out the whole DIV that covers the website.
                 $("#preloader").delay(300).fadeOut("slow");
             }); 
             
-            // for hero content animations 
+            //  hero content animations 
             $("html").removeClass('ss-preload');
             $("html").addClass('ss-loaded');
         
@@ -96,7 +95,7 @@
 
     };
 
-   /* Highlight the current section in the navigation bar
+   /*  navigation bar
     * ------------------------------------------------------ */
     var ssWaypoints = function() {
 
